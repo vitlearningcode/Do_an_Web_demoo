97f8788f8bf2bdc7e46042347722301a20f61ba5
ThanhHoa-DH24TH1 <hoa_dth235656@student.agu.edu.vn>
2026-03-15 22:09:37 +0700

    điều chỉnh csdl.sql

--- csdl.sql
+++ csdl.sql
@@ -1,5 +1,5 @@
 1.create table NguoiDung
-- maNd
+- maNd(khoa chinh)
 - tenNd
 - diachi
 - sdt
@@ -7,97 +7,119 @@
 
 
 2.create table TaiKhoan
-- tenDN
+- tenDN(khoa chinh)
 - matKhau
+- maNd(khoa ngoai)
+- mavt(khoa ngoai)
 
 3.create table vaiTro
-- mavt
+- mavt(khoa chinh)
 - tenvt
 
+
 4.create table Sach
-- maSach
-- theloai
-- giaNhap
-- soLuongNhap
-- soLuongTon
-- giaBan
-- khuyenMai
-- trangThai - NgungKD - DangKD
-
-
-5.create table chitietthongtinsach
+- maSach(khoa chinh)
 - tenSach
+- AnhSach
 - namSX
 - ngonNgu
 - moTaSach
-- tenNXB
-- tenTG
 - loaiBiaSach (bia mem - bia cung)
+- soLuongTon
+- giaBan
+- khuyenMai
+- trangThai (NgungKD - DangKD) 
 
+5.create table Sach_TheLoai
+- maSach(khoa chinh)
+- maTheLoai(khoa chinh)
 
-6.create table ncc
-- mancc
+6.create table Sach_TacGia
+- maSach(khoa chinh)
+- maTG(khoa chinh)
+
+7.create table Sach_NXB
+- maSach(khoa chinh)
+- maNXB(khoa chinh)
+
+8.create table NXB
+- maNXB(khoa chinh)
+- tenNXB
+- diachi
+
+9.Create table TacGia
+- maTG(khoa chinh)
+- tenTG
+- tieuSu
+
+10.Create table TheLoai
+- maTheLoai(khoa chinh)
+- tenTheLoai
+
+11.create table ncc
+- mancc(khoa chinh)
 - tenNCC
 - diachi
 - sdt
 - email
 - mức Chiếc khấu 
 
-7.create table PhieuNhap
-- maPn
-- tenNCC 
+12.create table PhieuNhap
+- maPn(khoa chinh)
 - tongLuongNhap
 - ngaylap
 - sotienDaThanhToan
 - tongtien
-- trangThai - ENG 
+- trangThai (Completed/Waiting/returned) 
+- mancc(khoa ngoai)
 
-8.create table chiTietPN
-- maPn
-- mancc
-- masach
+13.create table chiTietPN
+- maPn(khoa chinh)
+- masach(khoa chinh)
 - soLuongNhap
 - giaNhap
 - mức Chiếc khấu 
 - thanhTien
 
-9.create table PhieuXuat
-- maPX
+14.create table PhieuXuat
+- maPX(khoa chinh)
 - ngaylap
 - tongLuongXuat
 - tongtien
 
 
-10.create table chiTietPX
-- maPX
-- maNd
-- masach
+15.create table chiTietPX
+- maPX(khoa chinh)
+- maNd(khoa ngoai)
+- masach(khoa ngoai)
 - soLuong
 - giaBan
 - tiengiam float
 - thanhTien
 
-11.create table DoanhThu 
-- maDT
+16.create table DoanhThu 
+- maDT(khoa chinh)
 - thoigian
 - lợi nhuận 
 - DoanhThu
 - soDon
 
-12.create table congno
-- maCn
+17.create table congno
+- maCn(khoa chinh)
 - tieno
 - thoihan
 
-13.create table CTCN
-- maCn
-- maPn
-- 
+18.create table CTCN
+- maCn(khoa chinh)
+- maPn(khoa chinh)
+- mancc(khoa chinh)
 - sotienchuathanhtoan
 - thoihanno
 
-14.create table DanhGiaSach
-- mucDanhGia
+19.create table DanhGiaSach
+- maDGS(khoa chinh)
+- diemDG(1*->5*)
 - nhanXet
-- soLuongDG
+- maSach(khoa ngoai)
+- maNd(khoa ngoai)
 
