874d21109c4151d1d1ee21f6a8e3e7d6327c2526
Duck <leduc071204@gmail.com>
2026-04-10 18:25:36 +0700

    sửa lại cái nút cho đẹp

--- DoAn/CuaHang/TrangBanHang/GiaoDien/header.php
+++ DoAn/CuaHang/TrangBanHang/GiaoDien/header.php
@@ -63,7 +63,10 @@
                     </div>
                 <?php else: ?>
                     <button class="action-btn" onclick="openLogin()">
-                        <i class="fas fa-user"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+                            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                            <circle cx="12" cy="7" r="4"></circle>
+                        </svg>
                         <span>Đăng nhập</span>
                     </button>
                 <?php endif; ?>
